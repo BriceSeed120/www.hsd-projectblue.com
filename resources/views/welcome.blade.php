2fc033acf4786a4d2fffdf38bac7baf3b6bb4f85
BriceSeed120 <nde.brice@seed120.com>
2022-06-27 17:45:30 +0100

    Tranings section update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,6 @@
             <div class="col-lg-7">
               <h2>HSD PROJECTBLUE, Institut de formation agréé en Afrique.            </h2>
               <div class="our-story">
-                {{-- <h4>Est 1988</h4> --}}
                 <h3>Qui sommes nous ?</h3>
                 <p>HSD PROJECTBLUE est l’organisme de formation qui propose l'offre la plus diversifiée en Afrique. Un large choix vous est proposé dans les domaines clés tels que le management, le développement personnel, la vente, la négociation commerciale, le marketing...
                     En vous formant, vous bénéficierez de notre pédagogie innovante avec des solutions à distance et hybrides combinant formations présentielles et e-learning à travers l’Afrique.
@@ -21,15 +20,6 @@
                     <em>« La formation est l’essence même du succès »</em> <b>Arnaud BOTI</b> 
                     </p>
                 <ul>
-                  {{-- <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commo</span></li>
-                  <li><i class="bi bi-check-circle"></i> <span>Duis aute irure dolor in reprehenderit in</span></li>
-                  <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
-                </ul>
-                <p>Vitae autem velit excepturi fugit. Animi ad non. Eligendi et non nesciunt suscipit repellendus porro in quo eveniet. Molestias in maxime doloremque.</p>
-  
-                <div class="watch-video d-flex align-items-center position-relative">
-                  <i class="bi bi-play-circle"></i>
-                  <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox stretched-link">Watch Video</a> --}}
                 </div>
               </div>
             </div>
@@ -39,4 +29,232 @@
         </div>
       </section>
       <!-- End About Section -->
+      <!-- ======= Constructions Section ======= -->
+    <section id="constructions" class="constructions">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>Nos Solutions</h2>
+          {{-- <p>Nulla dolorum nulla nesciunt rerum facere sed ut inventore quam porro nihil id ratione ea sunt quis dolorem dolore earum</p> --}}
+        </div>
+
+        <div class="row gy-4">
+          
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="card-item">
+              <div class="row">
+                <div class="col-xl-5">
+                  <div class="card-bg" style="background-image: url(assets/img/solution-1.jpg);"></div>
+                </div>
+                <div class="col-xl-7 d-flex align-items-center">
+                  <div class="card-body">
+                    <h4 class="card-title">Formation certifiante ou diplômantes</h4>
+                    <p>13 programmes de formation, plus de 91 certifiantes ou diplômantes, vous sont proposés par HSD PROJECTBLUE.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="card-item">
+              <div class="row">
+                <div class="col-xl-5">
+                  <div class="card-bg" style="background-image: url(assets/img/solution-2.jpg);"></div>
+                </div>
+                <div class="col-xl-7 d-flex align-items-center">
+                  <div class="card-body">
+                    <h4 class="card-title">Formation sur-mesure</h4>
+                    <p>Avec 300 entreprises clientes, 183 formations proposées sur-mesure par an et 93 projets déployés dans 20 pays, quel que soit votre enjeu de compétences, HSD PROJECTBLUE vous accompagne. 
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="card-item">
+              <div class="row">
+                <div class="col-xl-5">
+                  <div class="card-bg" style="background-image: url(assets/img/solution-3.jpg);"></div>
+                </div>
+                <div class="col-xl-7 d-flex align-items-center">
+                  <div class="card-body">
+                    <h4 class="card-title">Formation e-learning</h4>
+                    <p>HSD PROJECTBLUE vous propose une offre de formation variée multi-supports, multi formats et multilingues pour un apprentissage encore plus efficace, à coût optimisé et accessible à tous. 
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="card-item">
+              <div class="row">
+                <div class="col-xl-5">
+                  <div class="card-bg" style="background-image: url(assets/img/solution-4.jpg);"></div>
+                </div>
+                <div class="col-xl-7 d-flex align-items-center">
+                  <div class="card-body">
+                    <h4 class="card-title">Formation inter entreprise </h4>
+                    <p>HSD PROJECTBLUE propose des formations pouvant regrouper des salariés de différentes entreprises au sein d'une même session.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Card Item -->
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="card-item">
+              <div class="row">
+                <div class="col-xl-5">
+                  <div class="card-bg" style="background-image: url(assets/img/solution-5.jpg);"></div>
+                </div>
+                <div class="col-xl-7 d-flex align-items-center">
+                  <div class="card-body">
+                    <h4 class="card-title">Formation intra-entreprise</h4>
+                    <p>HSD PROJECTBLUE peut proposer une formation existante au catalogue et délivrée exclusivement à un groupe ciblé de collaborateurs d'une même entreprise.
+
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><!-- End Card Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Constructions Section -->
+    <!-- ======= Alt Services Section ======= -->
+    <section id="alt-services" class="alt-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row justify-content-around gy-4">
+          <div class="col-lg-6 img-bg" style="background-image: url(assets/img/alt-advantages.jpg);" data-aos="zoom-in" data-aos-delay="100"></div>
+
+          <div class="col-lg-5 d-flex flex-column justify-content-center">
+            <h3>AVANTAGES</h3>
+            {{-- <p>Esse voluptas cumque vel exercitationem. Reiciendis est hic accusamus. Non ipsam et sed minima temporibus laudantium. Soluta voluptate sed facere corporis dolores excepturi</p> --}}
+
+            <ul style="">
+              <li ><i class="bi bi-check-circle"></i> <span>Des formateurs experts dans leurs domaines</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Un large réseau de formateurs certifiés</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Un délai de réponse rapide</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Des contenus adaptés</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Des méthodes pédagogiques avec un suivi post-formation</span></li>
+              <li><i class="bi bi-check-circle"></i> <span>Des méthodes d’évaluation</span></li>
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Alt Services Section -->
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h2>Nos Formations</h2>
+          {{-- <p>Nulla dolorum nulla nesciunt rerum facere sed ut inventore quam porro nihil id ratione ea sunt quis dolorem dolore earum</p> --}}
+        </div>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-item  position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-helmet-safety"></i>
+              </div>
+              <h3>Conditions de travail</h3>
+              <p>Améliorer les conditions de travail et la sécurité des salariés à
+                travers la réduction des accidents en milieu professionnel en
+                renforçant les compétences des employés sur les notions de
+                secourisme, lutte incendie, gestion des produits dangereux,
+                gestion crise sanitaire, etc.</p>
+                {{-- Call to undefined function link_to_asset() (View: D:\Seed120\HSD\ProjectBlue\Website\www.hsd-projectblue.com\resources\views\welcome.blade.php) --}}
+              <a href="url(assets/files/file.pdf)" class="readmore stretched-link">En savoir plus <i class="bi bi-arrow-right"></i></a>
+              {{-- <embed src="url(assets/files/file.pdf)" type=""> --}}
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-cart-shopping"></i>
+                {{-- <i class="fa-solid fa-arrow-up-from-ground-water"></i> --}}
+              </div>
+              <h3>Formations Commerciales</h3>
+              <p>La formation commerciale peut intégrer des sujets techniques
+                comme la prospection, la vente, la négociation et la relation
+                client mais aussi des thématiques plus spécifiques aux décideurs
+                comme le management d’équipe ou l’élaboration d’une stratégie
+                commerciale.</p>
+              <a href="url(asset('files/Catalogue Formation HSD - ProjectBlue 2022 - Original.pdf'))" class="readmore stretched-link">En savoir plus <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-people-line"></i>
+              </div>
+              <h3>Formations en Management</h3>
+              <p>Dans un contexte de transformation digitale et de recherche
+                d’agilité, la capacité du manager à accompagner les
+                changements et à faire preuve de leadership est désormais
+                essentielle afin de rendre ses équipes efficaces. Le manager agile
+                doit créer une dynamique de collaboration et travailler avec des
+                générations différentes. </p>
+              <a href="url(asset('files/Catalogue Formation HSD - ProjectBlue 2022 - Original.pdf'))" class="readmore stretched-link">En savoir plus <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-people-group"></i>
+              </div>
+              <h3>Ressources Humaines</h3>
+              <p>La formation en RH consiste à acquérir les fondamentaux qui
+                permettront, au sein du service RH, de contribuer à atteindre des
+                objectifs fixés par l’entreprise.</p>
+              <a href="url(asset('files/Catalogue Formation HSD - ProjectBlue 2022 - Original.pdf'))" class="readmore stretched-link">En savoir plus <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-basket-shopping"></i>
+              </div>
+              <h3>Formations Achats</h3>
+              <p>Les formations achats vous permettront d’acquérir des
+                compétences techniques de l’acheteur, notamment l’aisance
+                relationnelle, indispensable pour mener des négociations
+                fructueuses et obtenir les meilleures prestations à moindre coût.</p>
+              <a href="url(asset('files/Catalogue Formation HSD - ProjectBlue 2022 - Original.pdf'))" class="readmore stretched-link">En savoir plus <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-calculator"></i>
+              </div>
+              <h3>Formations Finance/Comptabilité</h3>
+              <p>L’offre de formation en comptabilité est tournée vers les besoins
+                opérationnels des comptables et financiers et des chefs
+                comptables. Nos deux cycles de formation abordent les bases de
+                la comptabilité</p>
+              <a href="url(asset('files/Catalogue Formation HSD - ProjectBlue 2022 - Original.pdf'))" class="readmore stretched-link">En savoir plus <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
 @endsection
